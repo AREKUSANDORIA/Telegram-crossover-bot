28a1cbb4cd8e37d440508b5222d6f6974652555e
AREKUSANDORIA <Lionelboukaka0@gmail.com>
2025-07-21 22:23:57 +0100

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,6 +1,11 @@
 from fastapi import FastAPI
+import uvicorn
+
 app = FastAPI()
 
 @app.get("/")
-def read_root():
+def home():
     return {"status": "Bot is alive"}
+
+def keep_alive():
+    uvicorn.run(app, host="0.0.0.0", port=10000)
